5af72a009ac86dc70c1f261f0839caed05e7c976
Mike Daley <michaelcdaley@yahoo.com>
2018-09-19 10:59:07 -0700

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+//-----------------------------------------------------------------------------
+// src/pages/about.js
+//-----------------------------------------------------------------------------
+import React    from 'react'
+import { Link } from 'gatsby'
+
+import Header from '../components/header'
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Header headerText = 'Dude, About Gatsby' />
+    <Header headerText = 'This is pretty cool' />
+    <p>Such wow. Very React.</p>
+  </div>
+)
