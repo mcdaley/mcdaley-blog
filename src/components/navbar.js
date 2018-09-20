09786284e5d293608254737c11c6b4938b28af30
Mike Daley <michaelcdaley@yahoo.com>
2018-09-19 18:16:36 -0700

    Fixed bug where clicking on the title caused an error message in the console by setting the path in the link to exact

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,7 +7,7 @@ import { Link } from 'gatsby'
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <Link to="/" className="navbar-brand">
+      <Link exact to="/" className="navbar-brand">
         BLOOGER
       </Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
