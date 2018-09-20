9370e65cf4435b43a4ca42882fdbb5a135a94284
Mike Daley <michaelcdaley@yahoo.com>
2018-09-19 18:10:27 -0700

    Added plugin to read source files and print out details about all the files in the src/ directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,12 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-sass`,
+    {
+      resolve:  `gatsby-source-filesystem`,
+      options:  {
+        name:   `src`,
+        path:   `${__dirname}/src/`,
+      },
+    },
   ],
 }
