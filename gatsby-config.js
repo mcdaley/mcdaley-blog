77fbdedbb8dfec4344ba71d853c1ed5792f32e2b
Mike Daley <michaelcdaley@gmail.com>
2018-09-20 13:20:14 -0700

    Moved test blog posts from src/pages to content

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,8 +11,8 @@ module.exports = {
     {
       resolve:  `gatsby-source-filesystem`,
       options:  {
-        name:   `src`,
-        path:   `${__dirname}/src/`,
+        name:   `content`,
+        path:   `${__dirname}/content/`,
       },
     },
   ],
