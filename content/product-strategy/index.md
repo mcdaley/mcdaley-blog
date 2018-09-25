f5647bc473c5453f89dc633d6fa968c51eb9516f
Mike Daley <michaelcdaley@gmail.com>
2018-09-25 06:01:05 -0700

    Added the gatsby plugins, gatsby-plugin-sharp and gatsby-remark-image. I then had to change the gatsby-config.js to configure the plugins correctly - as well as the gatsby-transformer-remark plugin. Verified that I can show an image by adding one of the robot images to a post

--- content/product-strategy/index.md
+++ content/product-strategy/index.md
@@ -1,9 +1,25 @@
 ---
 title:  "What is your product strategy"
-path:   "dude-what-is-your-product-strategy"
+path:   "what-is-your-product-strategy"
 date:   "2018-09-23"
 ---
 
+## Overview
 Having a clear, simple product strategy will help you build the right 
 features. Otherwise, you are going to spend a lot of time building things
 that will not drive your business.
+
+## Code
+Do not forget to add the code
+
+```javascript
+foreach i in array {
+  print "Counter = ${i}"
+}
+```
+## Working with Robots
+Here is a picture of a cute robot.
+
+<div>
+  <img src="./icon_robots_8.png" alt="Icon Robot" />
+</div>
--- gatsby-config.js
+++ gatsby-config.js
@@ -7,8 +7,24 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-sass`,
-    `gatsby-transformer-remark`,
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve:  `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve:  `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 1024,
+            },
+          },  
+        ],
+      },
+    },
     {
       resolve:  `gatsby-source-filesystem`,
       options:  {
