2071890cd41e7f88d377b1f76bf2f122c699ab92
Mike Daley <michaelcdaley@gmail.com>
2018-09-27 14:08:13 -0700

    Installed fontawesome free images package and setup basic social icons in the blogger-info section

--- src/components/blogger-info.js
+++ src/components/blogger-info.js
@@ -2,6 +2,7 @@
 // src/components/blogger-info.js
 //-----------------------------------------------------------------------------
 import React      from 'react'
+import { Link }   from 'gatsby'
 import Img        from 'gatsby-image'
 
 export default function BloggerInfo({ data }) {
@@ -11,6 +12,17 @@ export default function BloggerInfo({ data }) {
       <h4 className="text-left" style={{ marginTop: "1.50rem"}}>Michael Daley</h4>
       <p className="tagline">
         Connecting the dots between product management and software development
+      </p>
+      <p>
+        <a href="https://github.com/mcdaley" className="social-icon" target="_blank" rel="noopener noreferrer">          
+          <i className="fab fa-github fa-2x"></i>
+        </a>
+        <a href="https://linkedin.com/in/mcdaley" className="social-icon" target="_blank" rel="noopener noreferrer">
+          <i className="fab fa-linkedin fa-2x"></i>
+        </a>
+        <Link to="/contact" className="social-icon">
+          <i className="far fa-envelope fa-2x"></i>
+        </Link>
       </p>
     </div>
   )
